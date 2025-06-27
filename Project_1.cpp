7ccd4d27729c171f0f68f31415cd15fedb8af743
measam2008 <measamhaider2008@gmail.com>
2025-06-27 14:04:31 +0500

    Concept of cout and cin and performing sum and product of two numbers.

---
+++ Project_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    float Num_1,Num_2,sum,product;
+    cout<<"Enter value of Num_1: "<<endl;
+    cin>>Num_1;
+    cout<<"Enter value of Num_2: "<<endl;
+    cin>>Num_2;
+    sum=Num_1+Num_2;
+    cout<<"Sum: "<<sum<<endl;
+    product=Num_1*Num_2;
+    cout<<"Product: "<<product<<endl;
+    
+    return 0;
+}
