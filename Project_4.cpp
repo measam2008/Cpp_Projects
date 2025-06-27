44df9825d377297c27c214722d4f99d112a77511
measam2008 <measamhaider2008@gmail.com>
2025-06-27 22:47:51 +0500

    Parameter and Area of Trapezoid

---
+++ Project_4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   float parameter,area,a,b,c,d,h;
+   cout<<"Enter value of a: "<<endl;
+   cin>>a;
+   cout<<"Enter value of b: "<<endl;
+   cin>>b;
+   cout<<"Enter value of c: "<<endl;
+   cin>>c;
+   cout<<"Enter value of d: "<<endl;
+   cin>>d;
+   cout<<"Enter value of h: "<<endl;
+   cin>>h;
+   parameter=a+b+c+d;
+   cout<<"parameter of trapezoid:"<<parameter<<endl;
+   area=h*a*0.5+h*b*0.5;
+   cout<<"area of trapezoid: "<<area<<endl;
+    return 0;
+}
