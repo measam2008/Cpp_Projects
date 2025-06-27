2d70db2efef5557aa04e9048820849ef5d075002
measam2008 <measamhaider2008@gmail.com>
2025-06-27 22:44:12 +0500

    Area of Parallelogram-Concept of cin and cout.

---
+++ Project_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ float parameter,a,b,h,area;
+cout<<"Enter value of a"<<endl;
+cin>>a;
+cout<<"Enter value of b"<<endl;
+cin>>b;
+cout<<"Enter value of h"<<endl;
+cin>>h;
+parameter=2*a+2*b;
+cout<<"Parameter of Parallelogram:"<<parameter<<endl;
+area=b*h;
+cout<<"Area of Parallelogram: "<<area;
+return 0;}
