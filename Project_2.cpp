d3beec1e8454df0b7ef6f77d7ec43910c74585a1
measam2008 <measamhaider2008@gmail.com>
2025-06-27 22:36:21 +0500

    Concept of Arithmetic Operators(+,-,*,/,%)

---
+++ Project_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x ;
+  
+    float r;
+    cout<<"Enter you values:  \n";
+    cout<<"enter x: "<<endl;
+    cin>>x;
+    r = x+5;
+    cout<<"Result : "<<r<<endl;
+    r = x-5;
+    cout<<"Result : "<<r<<endl;
+    r = x*5;
+    cout<<"Result : "<<r<<endl;
+    r = x/5;
+    cout<<"Result : "<<r<<endl;
+    r = x%5;
+    cout<<"Result : "<<r<<endl;
+    
+
+    return 0;
+}
