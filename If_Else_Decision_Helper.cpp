5e65524a72dc3dd924014b06eb60bbced5c24c48
measam2008 <measamhaider2008@gmail.com>
2025-07-09 23:13:08 +0500

    Alarm Decession Helper By using If/Else and logical optr &&

---
+++ If_Else_Decision_Helper.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+   char weekday, exam, sick;
+   cout<<"Type 'Y' for 'Yes' and 'N' for 'No'! "<<endl;
+   
+   cout<<"Is it a weekday?(y/n) ";
+   cin>>weekday;
+   cout<<"Do you have exam tomorrow?(y/n) ";
+   cin>>exam;
+   cout<<"Are you feeling sick?(y/n) ";
+   cin>>sick;
+   
+   if(weekday=='Y' && weekday=='y' && exam=='Y' &&  exam=='y' && sick=='N' && sick=='n')
+   {
+       cout<<"Set alarm for 6 AM.";
+   }
+   else
+   {
+       cout<<"No need to set alarm.";
+   }
+    
+    return 0;
+}
