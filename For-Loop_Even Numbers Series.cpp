84a96f7e0c4853805d55d7cb65cb1285adc927e8
measam2008 <measamhaider2008@gmail.com>
2025-08-27 19:38:02 +0500

    Add program to calculate the sum of even numbers up to a user-defined limit

---
+++ For-Loop_Even Numbers Series.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    cout<<"Enter ending even number: ";
+    cin>>y;
+    z=0;
+    for (x=1 ; x<=y ; x++)
+    {
+        if (x%2==0)
+        {
+            cout<<x;
+            if (x<=(y-2))
+            {
+                cout<<" + ";
+            }
+            
+            z+=x;
+            
+        }
+    }
+    
+    cout<<"= "<<z;
+    
+    return 0;
+}
