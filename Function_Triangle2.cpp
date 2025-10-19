ac611d181ebbf9eb16c52b9c76383e3d8dd3ff1a
measam2008 <measamhaider2008@gmail.com>
2025-10-19 17:40:19 +0500

    Efficiency Improved....

--- Function_Triangle2.cpp
+++ Function_Triangle2.cpp
@@ -1,38 +1,36 @@
 #include <iostream>
 using namespace std;
 int rows(int);
-int sqr(int);
+int sqr(int,int);
 
 int main()
 {
-    int x;
+    int x,y;
     x=0;
-    sqr(x);
+    y=5;
+    sqr(x,y);
 
 
     return 0;
 }
 
-int sqr(int n)
+int sqr(int n,int rn)
 {
     if (n==5)
     {
         return 0;
     }
-    else{
-    rows(n);
-    cout<<endl;
-    return sqr(n+1);
-    }
-}
-int rows(int r)
-{
-    if (r==5)
+    else
     {
-        return 0;
-    }
-    else{
-    cout<<"*";
-    return rows(r+1);
+        if(rn==n)
+        {
+            cout<<endl;
+            return sqr(n+1,5);
+        }
+        else
+        {
+            cout<<"*";
+            return sqr(n,rn-1);
+        }
     }
 }
